ff26afaf211868347178e003a7cb21d5509f4792
Avinash <avi@dhumaketu.local>
2015-10-02 19:59:21 +0530

    more

---
+++ common/session/addr.js
@@ -0,0 +1,54 @@
+var addr = {};
+
+addr.prepend = function (a, resource, instance) {
+	var _a = a;
+	var _instance = '';
+
+	if (!_a)
+		_a = '';
+
+	if (instance)
+		_instance = ':' + instance;
+
+	return resource + _instance + _a;
+};
+
+addr.append = function (a, resource, instance) {
+	var _a = a;
+	var _instance = '';
+
+	if (!_a)
+		_a = '';
+
+	if (instance)
+		_instance = ':' + instance;
+
+	return _a + '.' + resource + _instance;
+};
+
+addr.inspect_top = function (a) {
+	if (!a)
+		return null;
+
+	var _a = a.split('.');
+	var _o = _a[0].split(':');
+	var _r = _o[0];
+	var _i = _o[1];
+
+	return {
+		resource : _r,
+		instance : _i
+	};
+};
+
+addr.pop = function (a) {
+	if (!a)
+		return null;
+
+	var _a = a.split('.');
+
+	_a.splice(0, 1);
+	return _a.join('.');
+};
+
+module.exports = addr;
