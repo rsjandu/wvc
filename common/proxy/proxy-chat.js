3e603d0eeaab8840644074c74fd257125ac7f1b7
Ubuntu <wiziq@ubuntu-a.wiziq-ubuntu-a.r9.internal.cloudapp.net>
2015-12-02 14:16:00 +0000

    Changes in proxy routes for lets-chat

--- common/proxy/proxy-chat.js
+++ common/proxy/proxy-chat.js
@@ -8,27 +8,21 @@ var proxy   = new require('redbird')({
 });
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-if (!process.argv[2] || !process.argv[3] || !process.argv[4]) {
-	console.log ('Usage: node proxy-chat.js <hostname> <ext-port> <int-port>');
+if (!process.argv[2]) {
+	console.log ('Usage: node proxy-chat.js <hostname>');
 	process.exit (1);
 }
 
 var host = process.argv[2];
-var ext_port = process.argv[3];
-var int_port = process.argv[4];
+var ext_port = 443;
 
-console.log ('Starting proxy for CHAT server @' + host + ':' + ext_port + ' -> localhost:' + int_port);
+console.log ('Starting proxy for CHAT server @ ' + host + ':' + ext_port);
 
 /*
  * Routes for the landing page */
 proxy.register(host + '/landing/', "localhost:2178/landing/");
-
 /*
  * Routes for the session cluster docker for 'test-internal' */
 proxy.register(host + '/session/test-internal', "localhost:7777/");
 
-proxy.register(host, "localhost:" + int_port);
+proxy.register(host + '/lets-chat/', "localhost:5000/");
