29b77d05bdd775991c3fc4282219fec7b42b508b
Avinash Bhatia <ab@3apples.co>
2015-12-10 14:32:12 +0530

    Added browser check

--- common/apps/landing/routes/session-v1.js
+++ common/apps/landing/routes/session-v1.js
@@ -2,7 +2,10 @@ var express   = require('express');
 var path      = require('path');
 var log       = require('landing/common/log');
 var session   = require('landing/controllers/session-v1');
+var browser   = require('landing/lib/browser');
 var router    = express.Router();
+
+router.use(browser.check_compatibility);
 
 router.get ('/:session_id/', function(req, res, next) {
 	return session.load_page (req, res, next);
