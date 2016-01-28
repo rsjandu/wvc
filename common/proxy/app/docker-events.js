89429ae4e9e8573bfa925f560981833c81422f14
somanshu <somanshujindal@gmail.com>
2016-01-28 15:19:11 +0530

    docker-monitor error handling

--- common/proxy/app/docker-events.js
+++ common/proxy/app/docker-events.js
@@ -8,13 +8,21 @@ var key;
 var value;
 
 function dockermonitor () {
-	
+
 	docker_monitor({
-		
+
 		onContainerUp: function(container) {
 			log.info(container, "docker-start-info");
 			key = '/session/' + container.Name;
 			value = 'localhost:' + container.Ports[0].PublicPort + '/';
+			if (route_list.routes[key]){
+				if (route_list.routes[key] === value){
+					console.log("Route already exists");
+					return;
+				}
+				proxy.unregister(host + key);
+				route_list.remove_route(key);
+			}
 			proxy.register(host + key, value);
 			route_list.add_route(key, value);
 		},
@@ -22,6 +30,10 @@ function dockermonitor () {
 		onContainerDown: function (container) {
 			log.info(container, "docker-stop-info");
 			key = '/session/' + container.Name;
+			if (!route_list.routes[key]){
+				log.error({err:"Docker route not exists"},"error log");
+				return;
+			}
 			proxy.unregister(host + key);
 			route_list.remove_route(key);
 		}
