fbac7d59e92a205fafb0f01677e66b66d8c02911
Pawan-Bishnoi <pawanbishnoi@outlook.com>
2016-02-18 17:05:22 +0530

    att-list minor changes

--- common/apps/landing/public/javascripts/framework/v2/resources/att-list/scroll.js
+++ common/apps/landing/public/javascripts/framework/v2/resources/att-list/scroll.js
@@ -17,8 +17,12 @@ define(function(require){
 		scroll = $el.scrollTop(),
 		round = last_scroll < scroll ? Math.ceil : Math.floor;
 
+		if( last_scroll == scroll){		/* happens everytime but creates problems with last element */
+			return;
+		}
 		last_scroll = round( scroll/li_height) * li_height;
 		$el.scrollTop( last_scroll);
+		last_scroll = $el.scrollTop(); /* added to remove problem scrolling last element due to view height */
 	}
 
 	return scroll;
