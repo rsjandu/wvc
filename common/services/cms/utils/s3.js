20f3be104ee9d706f1ead839526662d6a98b8ba2
Pawan-Bishnoi <pawanbishnoi@outlook.com>
2016-03-02 18:41:30 +0530

    cms minor changes

--- common/services/cms/utils/s3.js
+++ common/services/cms/utils/s3.js
@@ -7,10 +7,10 @@ var s3 = {};
 
 var AWS_ACCESS_KEY	= config.s3.key ,
 	AWS_SECRET_KEY	= config.s3.secret ,
-	S3_BUCKET		= config.s3.bucket ,
+	BUCKET_NAME		= config.s3.bucket ,
 	KEY_NAME		= config.s3.dir ;
 
-var CONTENT_URL;
+var CONTENT_URL = 'https://' + BUCKET_NAME + '.s3.amazonaws.com/' + KEY_NAME;
 var EXPIRE_TIMESTAMP = 2000;
 
 aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
@@ -19,7 +19,7 @@ var _s3 = new aws.S3();
 s3.get_upload_url = function(info){
 	var _d 		= $.Deferred() ,
 		params	= {
-			Bucket: S3_BUCKET,
+			Bucket: BUCKET_NAME,
 			Key: KEY_NAME+'/'+ ( !info.dir ? '' : info.dir+'/' ) + info.name,
 			Expires: EXPIRE_TIMESTAMP,
 			ContentType: info.type,
@@ -33,7 +33,7 @@ s3.get_upload_url = function(info){
 			else{
 				_d.resolve({
 					upload_url	: data,
-					access_url	: CONTENT_URL+info.filename,
+					access_url	: CONTENT_URL+ '/' + info.name,
 					filename	: info.name
 				});
 			}
